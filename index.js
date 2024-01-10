0283ca375be5ddc88ee6e5d31c12f5a50d9d8d77
Maicon Cesar <maiconcesarx@hotmail.com>
2024-01-10 12:17:28 -0300

    Segundo commit

---
+++ index.js
@@ -0,0 +1,5 @@
+const prompt = require('prompt-sync')();
+
+console.log("Bem vindo heroi!\nAntes de começamos sua aventura precisamos saber seu nome!");
+let nome = prompt("insira o nome do heroi:");
+console.log(nome);
