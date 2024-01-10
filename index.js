f7ace94c59f91973074dbd14df9b9c7c031c1e17
Maicon Cesar <maiconcesarx@hotmail.com>
2024-01-10 12:36:02 -0300

    teste

--- index.js
+++ index.js
@@ -2,70 +2,72 @@ const prompt = require('prompt-sync')();
 
 console.log("Bem vindo heroi!\nAntes de começamos sua aventura precisamos saber seu nome!");
 let nome = prompt("insira o nome do heroi:");
-let xpUsuario = 0;
+let experiencia = 0;
 let xpDropado = 200;
 let nivel = "Ferro";
 let laco = 0;
+let opcao;
+let opcao2 = "";
 console.log("Pronto, com isso feito finalmente poderemos iniciar nova aventura!");
-do{
+while (laco != 1){
     console.log("Voce encontrou um inimigo!");
-    let decisao1 = prompt("Deseja lutar?\ns para lutar ou n para fugir: ");
-    while(dicisao1 != "end"){
-            switch(decisao1){
-            case "s": 
+    opcao = prompt("Deseja lutar?\ns para lutar ou n para fugir: ");
+    while (opcao != "end") {
+        switch (opcao) {
+            case "s":
                 console.log("Voce derrotou um inimigo e ganhou " + xpDropado + "experiencia.");
-                xpUsuario += xpDropado;
-                decisao1 ="end";
+                experiencia += xpDropado;
+                opcao = "end";
                 break;
             case "n":
                 console.log("Voce conseguiu fugir do inimigo!");
-                decisao1 = "end";
+                opcao = "end";
                 break;
             default:
                 console.log("Selecione uma opção valida!");
-                decisao1 = prompt("Deseja lutar?\ns para lutar ou n para fugir: ");
+                opcao = prompt("Deseja lutar?\ns para lutar ou n para fugir: ");
                 break;
         }
     }
     console.log("Deseja continuar a aventura?");
-    let decisao2 = prompt("s para continuar a aventura ou n para encerrar a aventura: ");
-    while(decisao2 != end){
-        switch(decisao2){
-            case "s": 
+    opcao2 = prompt("s para continuar a aventura ou n para encerrar a aventura: ");
+    while (opcao2 != "end") {
+        switch (opcao2) {
+            case "s":
                 console.log("Voce decidiu continuar sua aventura!");
-                decisao2 ="end";
+                opcao2 = "end";
                 break;
             case "n":
                 console.log("Voce encerrou sua aventura!");
-                decisao2 = "end";
+                opcao2 = "end";
                 laco = 1;
                 break;
             default:
                 console.log("Selecione uma opção valida!");
-                decisao2 = prompt("s para continuar a aventura ou n para encerrar a aventura: ");
+                opcao2 = prompt("s para continuar a aventura ou n para encerrar a aventura: ");
                 break;
         }
     }
-    if(xpUsuario<=1000){
+    if (experiencia <= 1000) {
         nivel = "Ferro";
-    }else if(xpUsuario>= 1001 && xpUsuario<=2000){
+    } else if (experiencia >= 1001 && experiencia <= 2000) {
         nivel = "Bronze";
-        xpDropado =400;
-    }else if(xpUsuario>= 2001 && xpUsuario<=5000){
+        xpDropado = 400;
+    } else if (experiencia >= 2001 && experiencia <= 5000) {
         nivel = "Prata";
         xpDropado = 600;
-    }else if(xpUsuario>= 5001 && xpUsuario<=7000){
+    } else if (experiencia >= 5001 && experiencia <= 7000) {
         nivel = "Ouro";
         xpDropado = 800;
-    }else if(xpUsuario>= 7001 && xpUsuario<=8000){
+    } else if (experiencia >= 7001 && experiencia <= 8000) {
         nivel = "Platina";
-    }else if(xpUsuario>= 8001 && xpUsuario<=9000){
-        nivel = "Ascendete";
-    }else if(xpUsuario>= 9001 && xpUsuario<=10000){
+    } else if (experiencia >= 8001 && experiencia <= 9000) {
+        nivel = "Ascendente";
+    } else if (experiencia >= 9001 && experiencia <= 10000) {
         nivel = "Imortal";
-    }else if(xpUsuario>= 10001){
+    } else if (experiencia >= 10001) {
         nivel = "Radiante";
     }
     console.log("O Herói de nome " + nome + " está no nível de " + nivel + "!");
-}while(laco != 1)
-console.log("O Herói de nome " + nome + " encerrou sua jornada no nível de " + nivel + "!\nXp total do Herói: " + xpUsuario);
+}
+console.log("O Herói de nome " + nome + " encerrou sua jornada no nível de " + nivel + "!\nXp total do Herói: " + experiencia);
